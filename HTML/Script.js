da68706a850a21f710de23f408956c725a05c3d3
Ashoka M <ashokashokm2001@gmail.com>
2026-07-15 22:37:32 +0530

    sample commit

---
+++ HTML/Script.js
@@ -0,0 +1,46 @@
+console.log("This is External JS File");
+
+stdDetails={
+    name:"ravi",
+    age:20,
+    college:"HITAM",
+    skills:["HTML","Python","Java"]
+}
+
+console.log(stdDetails);
+
+a=10;
+b="javascript"
+c=true 
+d=null 
+num=54125.5
+s=Symbol("hello")
+m=2365745607005432715567n
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof num);
+console.log(typeof z);
+console.log(typeof s);
+console.log(typeof m);
+
+
+
+let arr=[10,20,40,true, null]
+console.table(arr);
+
+
+arr.push(20) //insert element at ending of array
+arr.unshift("java") //insert element at starting of array 
+arr.pop()//remove Last  index values +9+
+
+arr.shift() //remove ZERO th index values
+
+console.log(arr);
+
+
+arr1= new Array(90,60,70,80)
+console.table(arr1);
+
